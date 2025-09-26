9fb2462be772f26507136b283037bf747974e5b3
default <zoe.wrubleski@stu.ocsb.ca>
2025-09-26 13:10:08 +0000

    properly formated Hello, World! program using go

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
+/*
+ * Author: Zoe Wrubleski
+ * Version: 1.0.0
+ * Date: 2025-09-26
+ * This is the traditional "Hello, World!" program.
+ */
+
 package main
 
 import "fmt"
 
 func main () {
+  //print out the greeting
   fmt.Println("Hello, World!")
+
+  fmt.Println("\nDone.")
 }
