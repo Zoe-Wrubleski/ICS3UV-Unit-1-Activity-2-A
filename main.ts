9f388a809a29ae3fd25922505b2f0ed8895d5002
default <zoe.wrubleski@stu.ocsb.ca>
2025-09-26 13:03:33 +0000

    properly formated the Hello World! code

---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Zoe Wrubleski
+ * @version 1.0.0
+ * @date 2025-09-26
+ * @fileoverview This is the traditional "Hello, World!" program.
+ */
+
+// print out the greeting
+console.log("Hello, World!");
+
+console.log("\nDone.");
